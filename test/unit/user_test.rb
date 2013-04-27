66242667880b466b73e55334f6bea9891c269f66
Ken Torry <kentorry@afrodesiamedia.com>
2013-04-26 20:51:49 -0500

    Fixed bug with user profile validation

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,42 +1,49 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "a user should enter a first name" do
-  # 	user = User.new
-  #   assert !user.save
-  #   assert !user.errors[:first_name].empty?
-  # end
+  test "a user should enter a first name" do
+  	user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
 
-  # test "a user should enter a last name" do
-  #   user = User.new
-  #   assert !user.save
-  #   assert !user.errors[:last_name].empty?
-  # end
+  test "a user should enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
 
-  # test "a user should enter a profile name" do
-  #   user = User.new
-  #   assert !user.save
-  #   assert !user.errors[:profile_name].empty?
-  # end
+  test "a user should enter a profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
 
-  # test "a user should have a unique profile name" do
-  #   user = User.new
-  #   user.profile_name = user(:ken).profile_name
+  test "a user should have a unique profile name without spaces" do
+    user = User.new(first_name: 'Ariel', last_name: 'Torry', email: 'ariel@afrodesiamedia.com')
+    user.password = user.password_confirmation = 'asdfasdf'
 
-  #   user(:ken)
-  #   assert !user.save
-  #   assert !user.errors[:profile_name].empty?
-  # end
+    user.profile_name = users(:ken).profile_name
 
-  # test "a user should have a profile name without spaces" do
-  #   user = User.new
-  #   user.profile_name = "My Profile with Spaces"
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
 
-  #   assert !user.save
-  #   assert !user.errors[:profile_name].empty?
-  #   assert user.errors[:profile_name].include?("Must be formatted correctly")
+  test "a user should have a profile name without spaces" do
+    user = User.new
+    user.profile_name = "My Profile with Spaces"
 
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("Must be formatted correctly")
+  end
 
-  # end
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'Ariel', last_name: 'Torry', email: 'ariel@afrodesiamedia.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+
+    user.profile_name = 'zorion_6'
+    assert user.valid?
+  end
 
 end
